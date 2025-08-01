74c1e610da2be0eebb67737024a5b61c6d6131a9
Piotr Palka <coding@pressplay-music.com>
2025-08-01 08:33:21 +0200

    implement channel iterators

--- src/buffer.rs
+++ src/buffer.rs
@@ -7,6 +7,7 @@ pub trait AudioBuffer<T: Sample> {
     fn num_frames(&self) -> usize;
     fn channel(&self, index: usize) -> Option<&[T]>;
     fn channel_mut(&mut self, index: usize) -> Option<&mut [T]>;
+
     fn clear(&mut self);
     fn add(&mut self, other: &dyn AudioBuffer<T>, _channel_layout: ChannelLayout) {
         for channel in 0..self.num_channels() {
@@ -22,6 +23,71 @@ pub trait AudioBuffer<T: Sample> {
             }
         }
     }
+}
+
+pub struct ChannelIter<'a, T: Sample> {
+    buffer: &'a dyn AudioBuffer<T>,
+    channel_index: usize,
+}
+
+impl<'a, T: Sample> ChannelIter<'a, T> {
+    pub fn new(buffer: &'a dyn AudioBuffer<T>, channel_index: usize) -> Self {
+        Self {
+            buffer,
+            channel_index,
+        }
+    }
+}
+
+impl<'a, T: Sample> Iterator for ChannelIter<'a, T> {
+    type Item = &'a [T];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.channel_index < self.buffer.num_channels() {
+            let channel = self.buffer.channel(self.channel_index);
+            self.channel_index += 1;
+            channel
+        } else {
+            None
+        }
+    }
+}
+
+pub fn iter_channels<'a, T: Sample>(buffer: &'a dyn AudioBuffer<T>) -> ChannelIter<'a, T> {
+    ChannelIter::new(buffer, 0)
+}
+pub struct ChannelIterMut<'a, T: Sample> {
+    buffer: &'a mut dyn AudioBuffer<T>,
+    channel_index: usize,
+}
+
+impl<'a, T: Sample> ChannelIterMut<'a, T> {
+    pub fn new(buffer: &'a mut dyn AudioBuffer<T>, channel_index: usize) -> Self {
+        Self {
+            buffer,
+            channel_index,
+        }
+    }
+}
+
+impl<'a, T: Sample> Iterator for ChannelIterMut<'a, T> {
+    type Item = &'a mut [T];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.channel_index < self.buffer.num_channels() {
+            let channel = self.buffer.channel_mut(self.channel_index);
+            self.channel_index += 1;
+            channel
+        } else {
+            None
+        }
+    }
+}
+
+pub fn iter_channels_mut<'a, T: Sample>(
+    buffer: &'a mut dyn AudioBuffer<T>,
+) -> ChannelIterMut<'a, T> {
+    ChannelIterMut::new(buffer, 0)
 }
 
 pub struct MultiChannelBuffer<T: Sample> {
--- src/processor.rs
+++ src/processor.rs
@@ -1,4 +1,8 @@
-use crate::{buffer::AudioBuffer, channel::ChannelLayout, sample::Sample};
+use crate::{
+    buffer::{iter_channels, iter_channels_mut, AudioBuffer},
+    channel::ChannelLayout,
+    sample::Sample,
+};
 
 pub trait Processor<T: Sample> {
     fn process(&mut self, context: &mut ProcessingContext<T>);
@@ -15,12 +19,10 @@ pub struct PassThrough;
 impl<T: Sample> Processor<T> for PassThrough {
     // TODO: find way to implement channel iterators
     fn process(&mut self, context: &mut ProcessingContext<T>) {
-        for channel in 0..context.input_buffer.num_channels() {
-            if let Some(input_channel) = context.input_buffer.channel(channel) {
-                if let Some(output_channel) = context.output_buffer.channel_mut(channel) {
-                    output_channel.copy_from_slice(input_channel);
-                }
-            }
+        for (input_channel, output_channel) in
+            iter_channels(context.input_buffer).zip(iter_channels_mut(context.output_buffer))
+        {
+            output_channel.copy_from_slice(input_channel);
         }
     }
 }
