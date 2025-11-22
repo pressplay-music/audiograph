ef8a8c4af6f0ddb8f970afe6df0c46ea711d2677
Piotr Palka <coding@pressplay-music.com>
2025-11-22 15:48:23 +0100

    create diamond like structures for benchmarking

--- benches/audiograph_bench/benches/audiograph_benchmark.rs
+++ benches/audiograph_bench/benches/audiograph_benchmark.rs
@@ -2,73 +2,145 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use std::hint::black_box;
 
 // Import directly from the public modules
-use audiograph::buffer::{FrameSize, MultiChannelBuffer};
+use audiograph::buffer::{AudioBuffer, FrameSize, MultiChannelBuffer};
 use audiograph::processor::{ProcessingContext, Processor};
 
-struct BenchProcessor;
+struct BenchProcessor {
+    increment: f32,
+}
+
+impl BenchProcessor {
+    fn new(increment: f32) -> Self {
+        Self { increment }
+    }
+}
 
 impl Processor<f32> for BenchProcessor {
     fn process(&mut self, context: &mut ProcessingContext<f32>) {
         for channel in context.channel_layout.iter() {
+            let input_channel = context.input_buffer.channel(channel).unwrap();
             let output_channel = context.output_buffer.channel_mut(channel).unwrap();
-            for sample in output_channel.iter_mut() {
-                *sample = 0.5;
+            for (out_sample, in_sample) in output_channel.iter_mut().zip(input_channel.iter()) {
+                *out_sample += in_sample + self.increment;
             }
         }
     }
 }
 
-fn create_small_graph() -> (
-    audiograph::DspGraph<f32>,
-    MultiChannelBuffer<f32>,
-    MultiChannelBuffer<f32>,
-) {
+fn create_diamond_graph(layers: usize, buffer_size: usize) -> (audiograph::DspGraph<f32>, usize) {
     use audiograph::*;
 
-    let mut graph = DspGraph::new(2, FrameSize(64), None);
-    let processor_node = graph
-        .add_processor(BenchProcessor, MultiChannelBuffer::new(2, FrameSize(64)))
-        .unwrap();
+    assert!(layers % 2 == 1, "Layers must be odd");
+    assert!(layers >= 3, "Need at least 3 layers");
 
-    graph
-        .connect(GraphNode::Input, processor_node.into(), None)
-        .unwrap();
-    graph
-        .connect(processor_node.into(), GraphNode::Output, None)
-        .unwrap();
+    let middle_layer = layers / 2;
+    let increment = 0.1;
 
-    let input = MultiChannelBuffer::new(2, FrameSize(64));
-    let output = MultiChannelBuffer::new(2, FrameSize(64));
+    let mut total_edges = 0;
+    let mut layer_sizes = Vec::new();
+    for layer in 0..layers {
+        if layer == 0 {
+            layer_sizes.push(2);
+            total_edges += 2;
+        } else if layer <= middle_layer {
+            let nodes_in_layer = 2_usize.pow(layer as u32 + 1);
+            layer_sizes.push(nodes_in_layer);
+            total_edges += nodes_in_layer * 2;
+        } else {
+            let nodes_in_layer = 2_usize.pow((layers - layer) as u32);
+            layer_sizes.push(nodes_in_layer);
+            total_edges += nodes_in_layer;
+        }
+    }
 
-    (graph, input, output)
+    let mut graph = DspGraph::new(2, FrameSize(buffer_size), Some(total_edges));
+
+    let mut all_layers = Vec::new();
+    for layer in 0..layers {
+        let nodes_in_layer = layer_sizes[layer];
+
+        let mut layer_nodes = Vec::new();
+        for _i in 0..nodes_in_layer {
+            let processor_node = graph
+                .add_processor(
+                    BenchProcessor::new(increment),
+                    MultiChannelBuffer::new(2, FrameSize(buffer_size)),
+                )
+                .unwrap();
+            layer_nodes.push(processor_node);
+        }
+        all_layers.push(layer_nodes);
+    }
+
+    for node in &all_layers[0] {
+        graph
+            .connect(GraphNode::Input, (*node).into(), None)
+            .unwrap();
+    }
+
+    for layer in 0..layers - 1 {
+        let current_layer = &all_layers[layer];
+        let next_layer = &all_layers[layer + 1];
+
+        if layer < middle_layer {
+            // Expanding phase
+            for (i, &current_node) in current_layer.iter().enumerate() {
+                let next_start = i * 2;
+                graph
+                    .connect(current_node.into(), next_layer[next_start].into(), None)
+                    .unwrap();
+                graph
+                    .connect(current_node.into(), next_layer[next_start + 1].into(), None)
+                    .unwrap();
+            }
+        } else {
+            // Contracting phase
+            for (i, &next_node) in next_layer.iter().enumerate() {
+                let current_start = i * 2;
+                graph
+                    .connect(current_layer[current_start].into(), next_node.into(), None)
+                    .unwrap();
+                graph
+                    .connect(
+                        current_layer[current_start + 1].into(),
+                        next_node.into(),
+                        None,
+                    )
+                    .unwrap();
+            }
+        }
+    }
+
+    // Connect last layer to output
+    let last_layer = &all_layers[layers - 1];
+    for node in last_layer {
+        graph
+            .connect((*node).into(), GraphNode::Output, None)
+            .unwrap();
+    }
+
+    (graph, total_edges)
 }
 
 fn bench_audiograph_performance(c: &mut Criterion) {
     let mut group = c.benchmark_group("audiograph_performance");
 
-    // Test different buffer sizes (advantage over DASP's fixed 64)
-    for &buffer_size in &[64, 256, 1024] {
-        use audiograph::*;
-
-        let mut graph = DspGraph::new(2, FrameSize(buffer_size), Some(10));
-        let processor_node = graph
-            .add_processor(
-                BenchProcessor,
-                MultiChannelBuffer::new(2, FrameSize(buffer_size)),
-            )
-            .unwrap();
-
-        graph
-            .connect(GraphNode::Input, processor_node.into(), None)
-            .unwrap();
-        graph
-            .connect(processor_node.into(), GraphNode::Output, None)
-            .unwrap();
-
+    // Test different diamond structures
+    for &layers in &[3, 5, 7, 9] {
+        let buffer_size = 64;
+        let (mut graph, total_edges) = create_diamond_graph(layers, buffer_size);
         let input = MultiChannelBuffer::new(2, FrameSize(buffer_size));
         let mut output = MultiChannelBuffer::new(2, FrameSize(buffer_size));
 
-        group.bench_function(&format!("buffer_size_{}", buffer_size), |b| {
+        // Check output value before benchmarking
+        graph.process(&input, &mut output, FrameSize(buffer_size));
+        println!(
+            "Audiograph {} layers -> output[0][0] = {}",
+            layers,
+            output.channel(0).unwrap()[0]
+        );
+
+        group.bench_function(&format!("graph_{}", total_edges), |b| {
             b.iter(|| {
                 graph.process(
                     black_box(&input),
--- benches/dasp_graph_bench/benches/dasp_benchmark.rs
+++ benches/dasp_graph_bench/benches/dasp_benchmark.rs
@@ -5,41 +5,203 @@ use std::hint::black_box;
 use dasp_graph::node::Node;
 use dasp_graph::{Buffer, NodeData};
 
-struct BenchProcessor;
+struct BenchProcessor {
+    increment: f32,
+}
 
-impl Node for BenchProcessor {
-    fn process(&mut self, _inputs: &[dasp_graph::Input], output: &mut [Buffer]) {
-        for buffer in output.iter_mut() {
-            for sample in buffer.iter_mut() {
-                *sample = 0.5; // Same fixed computational work as audiograph
+impl BenchProcessor {
+    fn new(increment: f32) -> Self {
+        Self { increment }
+    }
+}
+
+enum GraphNode {
+    Source,
+    Processor(BenchProcessor),
+    Output,
+}
+
+impl Node for GraphNode {
+    fn process(&mut self, inputs: &[dasp_graph::Input], outputs: &mut [Buffer]) {
+        match self {
+            GraphNode::Source => {
+                for output in outputs.iter_mut() {
+                    for sample in output.iter_mut() {
+                        *sample = 0.0;
+                    }
+                }
+            }
+            GraphNode::Processor(processor) => {
+                processor.process(inputs, outputs);
+            }
+            GraphNode::Output => {
+                for output in outputs.iter_mut() {
+                    for sample in output.iter_mut() {
+                        *sample = 0.0;
+                    }
+                }
+
+                for input in inputs.iter() {
+                    for (output_buffer, input_buffer) in
+                        outputs.iter_mut().zip(input.buffers().iter())
+                    {
+                        for (out_sample, in_sample) in
+                            output_buffer.iter_mut().zip(input_buffer.iter())
+                        {
+                            *out_sample += in_sample;
+                        }
+                    }
+                }
             }
         }
     }
 }
 
-type DaspGraph = petgraph::graph::DiGraph<NodeData<BenchProcessor>, ()>;
+impl Node for BenchProcessor {
+    fn process(&mut self, inputs: &[dasp_graph::Input], outputs: &mut [Buffer]) {
+        for output in outputs.iter_mut() {
+            for sample in output.iter_mut() {
+                *sample = 0.0;
+            }
+        }
+
+        // Sum all inputs into outputs (node's responsibility in DASP)
+        for input in inputs.iter() {
+            for (output_buffer, input_buffer) in outputs.iter_mut().zip(input.buffers().iter()) {
+                for (out_sample, in_sample) in output_buffer.iter_mut().zip(input_buffer.iter()) {
+                    *out_sample += in_sample;
+                }
+            }
+        }
+
+        // Add increment
+        for output in outputs.iter_mut() {
+            for sample in output.iter_mut() {
+                *sample += self.increment;
+            }
+        }
+    }
+}
+
+type DaspGraph = petgraph::graph::DiGraph<NodeData<GraphNode>, ()>;
 type DaspProcessor = dasp_graph::Processor<DaspGraph>;
 
-fn create_dasp_graph_setup() -> (DaspGraph, DaspProcessor, petgraph::graph::NodeIndex) {
-    let mut graph = DaspGraph::with_capacity(10, 10);
-    let processor = DaspProcessor::with_capacity(10);
+fn create_dasp_diamond_graph(
+    layers: usize,
+) -> (DaspGraph, DaspProcessor, petgraph::graph::NodeIndex, usize) {
+    assert!(layers % 2 == 1, "Layers must be odd");
+    assert!(layers >= 3, "Need at least 3 layers");
 
-    let node_data = NodeData::new2(BenchProcessor);
-    let node_id = graph.add_node(node_data);
+    let middle_layer = layers / 2;
+    let increment = 0.1;
 
-    (graph, processor, node_id)
+    let mut total_edges = 0;
+    let mut layer_sizes = Vec::new();
+    for layer in 0..layers {
+        if layer == 0 {
+            layer_sizes.push(2);
+            total_edges += 2;
+        } else if layer <= middle_layer {
+            let nodes_in_layer = 2_usize.pow(layer as u32 + 1);
+            layer_sizes.push(nodes_in_layer);
+            total_edges += nodes_in_layer * 2;
+        } else {
+            let nodes_in_layer = 2_usize.pow((layers - layer) as u32);
+            layer_sizes.push(nodes_in_layer);
+            total_edges += nodes_in_layer;
+        }
+    }
+
+    let total_nodes: usize = layer_sizes.iter().sum::<usize>() + 2; // +1 for source, +1 for output
+    total_edges += layer_sizes[0]; // source connects to first layer
+    total_edges += layer_sizes[layers - 1]; // last layer connects to output
+
+    let mut graph = DaspGraph::with_capacity(total_nodes, total_edges);
+    let processor = DaspProcessor::with_capacity(total_nodes);
+
+    let source_node_data = NodeData::new2(GraphNode::Source);
+    let source_node = graph.add_node(source_node_data);
+
+    let output_node_data = NodeData::new2(GraphNode::Output);
+    let output_node = graph.add_node(output_node_data);
+
+    // Create layers of nodes
+    let mut all_layers = Vec::new();
+    for layer in 0..layers {
+        let nodes_in_layer = layer_sizes[layer];
+
+        let mut layer_nodes = Vec::new();
+        for _i in 0..nodes_in_layer {
+            let node_data = NodeData::new2(GraphNode::Processor(BenchProcessor::new(increment)));
+            let node_id = graph.add_node(node_data);
+            layer_nodes.push(node_id);
+        }
+        all_layers.push(layer_nodes);
+    }
+
+    // Connect source to first layer (equivalent to Input → first layer)
+    for node in &all_layers[0] {
+        graph.add_edge(source_node, *node, ());
+    }
+
+    // Connect between layers
+    for layer in 0..layers - 1 {
+        let current_layer = &all_layers[layer];
+        let next_layer = &all_layers[layer + 1];
+
+        if layer < middle_layer {
+            // Expanding phase: each node connects to 2 nodes in next layer
+            for (i, &current_node) in current_layer.iter().enumerate() {
+                let next_start = i * 2;
+                graph.add_edge(current_node, next_layer[next_start], ());
+                graph.add_edge(current_node, next_layer[next_start + 1], ());
+            }
+        } else {
+            // Contracting phase: 2 nodes connect to each node in next layer
+            for (i, &next_node) in next_layer.iter().enumerate() {
+                let current_start = i * 2;
+                graph.add_edge(current_layer[current_start], next_node, ());
+                graph.add_edge(current_layer[current_start + 1], next_node, ());
+            }
+        }
+    }
+
+    // Connect last layer to output node (equivalent to last layer → Output)
+    for &last_node in &all_layers[layers - 1] {
+        graph.add_edge(last_node, output_node, ());
+    }
+
+    // Return output node as entry point
+    (graph, processor, output_node, total_edges)
 }
 
 fn bench_dasp_performance(c: &mut Criterion) {
     let mut group = c.benchmark_group("dasp_performance");
 
-    let (mut dasp_graph, mut dasp_processor, node_id) = create_dasp_graph_setup();
+    for &layers in &[3, 5, 7, 9] {
+        let (mut dasp_graph, mut dasp_processor, node_id, total_edges) =
+            create_dasp_diamond_graph(layers);
 
-    group.bench_function("full_graph_64_samples", |b| {
-        b.iter(|| {
-            dasp_processor.process(black_box(&mut dasp_graph), black_box(node_id));
-        })
-    });
+        // Check output value before benchmarking
+        dasp_processor.process(&mut dasp_graph, node_id);
+        let output_buffer = dasp_graph
+            .node_weight(node_id)
+            .unwrap()
+            .buffers
+            .first()
+            .unwrap();
+
+        println!(
+            "DASP {} layers -> output[0][0] = {}",
+            layers, output_buffer[0]
+        );
+
+        group.bench_function(&format!("graph_{}", total_edges), |b| {
+            b.iter(|| {
+                dasp_processor.process(black_box(&mut dasp_graph), black_box(node_id));
+            })
+        });
+    }
 
     group.finish();
 }
