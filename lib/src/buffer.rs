3feb96193867973703b7100b00f30b35709258ac
Piotr Palka <coding@pressplay-music.com>
2025-12-26 18:57:05 +0100

    fix iteration when adding buffers

--- lib/src/buffer.rs
+++ lib/src/buffer.rs
@@ -25,14 +25,14 @@ pub trait AudioBuffer<T: Sample> {
 
     fn add(&mut self, other: &dyn AudioBuffer<T>, channel_layout: &Option<ChannelLayout>) {
         if let Some(layout) = channel_layout {
-            // TODO: check if take() is used correctly here
-            for channel in layout.iter().take(self.num_channels()) {
-                if let (Some(src), Some(dst)) = (other.channel(channel), self.channel_mut(channel))
-                {
-                    dst.iter_mut().zip(src.iter()).for_each(|(a, b)| {
-                        *a += *b;
-                    });
-                }
+            let mut filtered_layout = layout.clone();
+            filtered_layout.clamp(self.num_channels().min(other.num_channels()));
+            for channel in filtered_layout.iter() {
+                let src = other.channel(channel).unwrap();
+                let dst = self.channel_mut(channel).unwrap();
+                dst.iter_mut().zip(src.iter()).for_each(|(a, b)| {
+                    *a += *b;
+                });
             }
         } else {
             let num_channels = self.num_channels().min(other.num_channels());
